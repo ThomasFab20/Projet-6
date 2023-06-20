b10400e1a0ed8dfdfa77fe534a2c2b860a083af0
ThomasFab20 <fabro.thomas95@gmail.com>
2023-06-20 13:10:55 +0200

    Ajout du filtre

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,7 +2,6 @@ let galerie = document.querySelector('.gallery');
 
 let image = -1;
 
-
 async function fetchProject(){
     let projet = await fetch('http://localhost:5678/api/works', {
     method: 'GET',
@@ -21,16 +20,48 @@ async function fetchProject(){
 
         imagesProjets.setAttribute('class', 'img-projet');
 
+        imagesProjets.classList.add('active');
+
+        imagesProjets.setAttribute('id', projets[image].category.name);
+
         galerie.appendChild(imagesProjets);
 
         imagesProjets.src = projets[image].imageUrl;
-        
     }
-
 }
     else{
         throw new Error('Impossible de récupérer la liste des projets');
 }}
 
 fetchProject();
+
+window.onload = () => {
+    let filters = document.querySelectorAll('.filter');
+
+    for(let filter of filters){
+        filter.addEventListener('click', function(){
+            let tag = this.id;
+
+            document.querySelector('.selected').classList.remove('selected');
+            this.classList.add('selected');
+
+            let images = document.querySelectorAll('.img-projet');
+
+            for(let image of images){
+                image.classList.replace('active', 'inactive')
+
+                if(tag === image.id){
+                    image.classList.replace('inactive', 'active');
+                }
+                if(tag === 'tous'){
+                    image.classList.replace('inactive', 'active');
+                }
+            }
+
+        });
+    }
+}
+
+
+
 
